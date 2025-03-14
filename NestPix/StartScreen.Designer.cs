54c0c853058ea1a9e9cd3ee9f7dc376f5e70adbc
Mahmoud Nasr <239.nasr@gmail.com>
2025-03-14 15:04:00 +0200

    Add project files.

---
+++ NestPix/StartScreen.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace NestPix
+{
+    partial class StartScreen
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            start = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Folder Path";
+            label1.TextAlign = ContentAlignment.BottomRight;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(110, 14);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(459, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(start);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 9);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 66);
+            panel1.TabIndex = 3;
+            // 
+            // start
+            // 
+            start.Location = new Point(575, 14);
+            start.Name = "start";
+            start.Size = new Size(213, 34);
+            start.TabIndex = 2;
+            start.Text = "Go";
+            start.UseVisualStyleBackColor = true;
+            start.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(800, 75);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Show;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "NestPix";
+            TopMost = true;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Button start;
+    }
+}
---
+++ NestPix/StartScreen.cs
@@ -0,0 +1,15 @@
+namespace NestPix
+{
+    public partial class StartScreen : Form
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
